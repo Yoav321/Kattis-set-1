b2f2fc17393554d5ada2a50c5264481321b53299
Yoav321 <yoav.bierkatz@gmail.com>
2026-09-10 09:41:29 -0600

    complete concatenate

---
+++ concatenate.py
@@ -0,0 +1,12 @@
+"""
+Combines two inputs
+Yoav Bierkatz, Jude Averitt, Kaplan Tonelli - September 2026
+"""
+
+def main() -> None:
+  s: str = str(input(""))
+  t: str = str(input(""))
+  print(f"{s}{t}")
+if __name__ == "__main__":
+  main()
+    
