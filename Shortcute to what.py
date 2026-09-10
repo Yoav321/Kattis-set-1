6e66650899f819adbcdf153698bac9de404d17bd
Judeaveritt <Judeaveritt@gmail.com>
2026-09-10 10:08:17 -0600

    complete a shortcut to what

--- Shortcute to what.py
+++ Shortcute to what.py
@@ -1,5 +1,5 @@
 """
-Describe your program.
+solves a shortcut to what 
 Yoav Bierkatz - September 2026
 """
 
@@ -7,12 +7,15 @@ def main() -> None:
   pass # remove me
 
   # input
+  n: int = int(input(""))
+
 
 
   # processing
   
   
   # output
+  print(f"{(5+n)*3-10}")
 
 
 if __name__ == "__main__":
