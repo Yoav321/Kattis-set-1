a3a195cc6cea7fecbf87cd0efbd2a120473db7e7
Yoav321 <yoav.bierkatz@gmail.com>
2026-09-10 10:04:47 -0600

    complete parking pandamoniun

---
+++ Parking_Pandemonium.py
@@ -0,0 +1,15 @@
+"""
+multiplies two numbers together
+Yoav Bierkatz - September 2026
+"""
+
+def main() -> None:
+  m: int = int(input(""))
+  k: int = int(input(""))
+  c: int = int(input(""))
+  print(f"{m*c}")
+
+
+if __name__ == "__main__":
+  main()
+    
